05d423e26e7d56a2f9899818ec045801ab1c59f1
ROBERT SCHAFER <robSchafer@ROBERTs-iMac.fios-router.home>
2018-08-30 23:16:07 -0400

    bot replies with reversed audio

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import os
 from telegram.ext import Filters, CommandHandler, MessageHandler, Updater
 import telegram as tg
 import logging
+#for calling ffmpeg and sox
+from subprocess import call
+
 
 
 log_level_env = os.environ.get("LOG_LEVEL")
@@ -12,6 +15,7 @@ elif log_level_env == "info":
     LOG_LEVEL=logging.INFO
 else:
     LOG_LEVEL=logging.INFO
+LOG_LEVEL=logging.INFO
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -36,9 +40,16 @@ def echo(bot, update):
     print(f"File id: {file_id}")
     newFile = bot.get_file(file_id)
     print(f"New file: {newFile}")
-    newFile.download('voice.ogg')
-    bot.send_message(chat_id=update.message.chat_id, text="Thanks for the audio clip. Soon I'll respond with ")
-    #bot.send_audio(chat_id=update.message.chat_id, audio=open('voice.ogg', 'rb'))
+    ogg_filename = 'voice.ogg'
+    wav_filename = 'voice.wav'
+    mp3_filename = 'voice.mp3'
+    wav_filename_reversed = 'voice_reversed.wav'
+    newFile.download(ogg_filename)
+    print(call(['ffmpeg','-y', '-i',ogg_filename,wav_filename]))
+    print(call(['sox', wav_filename, wav_filename_reversed,'reverse']))
+    print(call(['ffmpeg','-y', '-i',wav_filename_reversed,mp3_filename]))
+    #bot.send_message(chat_id=update.message.chat_id, text="Thanks for the audio clip. Soon I'll respond.")
+    bot.send_audio(chat_id=update.message.chat_id, audio=open(mp3_filename, 'rb'))
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
