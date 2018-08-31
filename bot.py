1e79fdfe393199cff144caa61059207babd1c4b7
ROBERT SCHAFER <robSchafer@ROBERTs-iMac.fios-router.home>
2018-08-30 22:45:24 -0400

    add echo function to download file

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ if log_level_env == "debug":
 elif log_level_env == "info":
     LOG_LEVEL=logging.INFO
 else:
-    LOG_LEVEL=logging.DEBUG
+    LOG_LEVEL=logging.INFO
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -28,9 +28,26 @@ dispatcher = updater.dispatcher
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+def echo(bot, update):
+    if update.message.voice is None:
+        bot.send_message(chat_id=update.message.chat_id, text="Send me an audio clip!")
+        return
+    file_id = update.message.voice.file_id
+    print(f"File id: {file_id}")
+    newFile = bot.get_file(file_id)
+    print(f"New file: {newFile}")
+    newFile.download('voice.ogg')
+    bot.send_message(chat_id=update.message.chat_id, text="Thanks for the audio clip. Soon I'll respond with ")
+    #bot.send_audio(chat_id=update.message.chat_id, audio=open('voice.ogg', 'rb'))
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+from telegram.ext import MessageHandler, Filters
+
+echo_handler = MessageHandler(Filters.all, echo)
+dispatcher.add_handler(echo_handler)
+
 print('test')
 updater.start_polling()
 updater.idle()
